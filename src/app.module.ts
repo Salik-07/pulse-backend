86af43ab1b83f8a96e6d17613f5c68d84a8943a1
Salik-07 <salikhussain41@gmail.com>
2025-10-15 20:34:06 +0500

    Add Auth, and Security module as core system functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './core/auth/auth.module';
+import { UsersModule } from './core/users/users.module';
+import { SecurityModule } from './core/security/security.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, SecurityModule],
   controllers: [AppController],
   providers: [AppService],
 })
